03c5337fce84f6e6031d2f9af75173edadbb2782
Christian Nagel <christian@christiannagel.com>
2020-10-13 13:03:39 +0200

    async and promises samples

---
+++ AsyncFunctionsSample/AsyncFunctionsSample/wwwroot/asyncsample.js
@@ -0,0 +1,22 @@
+﻿function resolveAfter5Seconds() {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            resolve("a result from the promise");
+        }, 5000);
+    });
+}
+
+function asyncCallOldVersion() {
+    console.log("starting async call");
+    resolveAfter5Seconds.then(function (result) {
+        console.log("async method completed, result: " + result);
+    });
+}
+
+async function asyncCallNewVersion() {
+    console.log("starting async call");
+    var result = await resolveAfter5Seconds();
+    console.log("async method completed, result: " + result);
+}
+
+asyncCallNewVersion();
---
+++ AsyncFunctionsSample/AsyncFunctionsSample/wwwroot/functionspage.js
@@ -0,0 +1,20 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("noarrowbutton").onclick = onnoarrow;
+    document.getElementById("arrowbutton").onclick = onarrow;
+});
+
+function onnoarrow() {
+    var add = function (x, y) {
+        return x + y;
+    }
+
+    var result = add(38, 4);
+    console.log(result);
+}
+
+function onarrow() {
+    var add = (x, y) => x + y;
+
+    var result = add(38, 4);
+    console.log(result);
+}
