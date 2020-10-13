9a04a4fd96dc6116e2804d5dc440d54d6c764369
Christian Nagel <christian@christiannagel.com>
2020-10-13 13:04:00 +0200

    fetch and xmlhttprequest samples

---
+++ APISample/APISample/wwwroot/UseXMLHttpRequestObject.js
@@ -0,0 +1,15 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("usexmlhttp").onclick = oldcallsample;
+});
+
+function oldcallsample() {
+    var request = new XMLHttpRequest();
+    request.open("GET", "WeatherForecast");
+    request.send();
+    request.onreadystatechange = function () {
+        if (request.readyState === 4) {
+            var response = JSON.parse(request.responseText);
+            console.log(response);
+        }
+    };
+}
---
+++ APISample/APISample/wwwroot/UsefetchAPI.js
@@ -0,0 +1,9 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("usefetch").onclick = newcallsample;
+});
+
+async function newcallsample() {
+    var response = await fetch("WeatherForecast");
+    var weather = await response.json();
+    console.log(weather);
+}
